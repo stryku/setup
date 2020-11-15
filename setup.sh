d210d94841fafd1e0c159e95c01f4bc9d7d836a3
stryku <stryku2393@gmail.com>
2020-11-15 16:41:38 +0100

    Got rid of function

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # TODO:
-# brave
 # virtualbox
 # docker
 # filezilla
@@ -20,74 +19,71 @@ function decode {
 # Go to a temp dir
 cd $(mktemp -d)
 
-function do_setup {
-    apt update && apt upgrade 
+# Remove firefox
+apt purge firefox -y
 
-    # Git
-    apt install git 
-    git config --global user.email "$(decode \"fgelxh2393@tznvy.pbz\")"
-    git config --global user.name "stryku"
+# Git
+apt install git -y
+git config --global user.email "$(decode \"fgelxh2393@tznvy.pbz\")"
+git config --global user.name "stryku"
 
-    git config --global alias.cm "commit -v -m"
-    git config --global alias.co "checkout"
-    git config --global alias.cb "checkout -b"
-    git config --global alias.aa "add --all ."
-    git config --global alias.st "status"
-    git config --global alias.last "log -1 HEAD"
-    git config --global alias.df "diff"
-    git config --global alias.ba "branch -a"
-    git config --global alias.bd "branch -d"
-    git config --global alias.mnf "merge --no-ff"
-    git config --global alias.addtl "commit --amend -C HEAD"
-    git config --global alias.ri "rebase -i"
-    git config --global alias.fp "fetch -p"
-    git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
-    git config --global alias.cmcount "rev-list --all --count"
-    git config --global alias.rsweep "remote update origin --prune"
-    git config --global alias.tl "tag --list"
-    git config --global alias.com "checkout master"
+git config --global alias.cm "commit -v -m"
+git config --global alias.co "checkout"
+git config --global alias.cb "checkout -b"
+git config --global alias.aa "add --all ."
+git config --global alias.st "status"
+git config --global alias.last "log -1 HEAD"
+git config --global alias.df "diff"
+git config --global alias.ba "branch -a"
+git config --global alias.bd "branch -d"
+git config --global alias.mnf "merge --no-ff"
+git config --global alias.addtl "commit --amend -C HEAD"
+git config --global alias.ri "rebase -i"
+git config --global alias.fp "fetch -p"
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global alias.cmcount "rev-list --all --count"
+git config --global alias.rsweep "remote update origin --prune"
+git config --global alias.tl "tag --list"
+git config --global alias.com "checkout master"
 
-    apt install gitk 
+apt install gitk -y
 
-    # Clone the setup repo
-    git clone https://github.com/stryku/setup && cd setup
+# Clone the setup repo
+git clone https://github.com/stryku/setup && cd setup
 
-    # Vim
-    apt install vim 
-    cp files/.viminfo ~/
+# Vim
+apt install vim -y
+cp files/.viminfo ~/
 
-    # Zsh
-    apt install zsh 
-    chsh -s $(which zsh)
-    cp files/.zshrc ~/
+# Zsh
+apt install zsh -y
+chsh -s $(which zsh)
+cp files/.zshrc ~/
 
-    # VS Code
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-    sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-    apt-get install apt-transport-https 
-    apt-get update
-    apt-get install code 
+# VS Code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt-get install apt-transport-https 
+apt-get update
+apt-get install code -y
 
-    # KeePassXC
-    apt install keepassxc 
+# KeePassXC
+apt install keepassxc -y
 
-    # DoubleCommander
-    apt install doublecmd-qt 
+# DoubleCommander
+apt install doublecmd-qt -y
 
-    # Brave
-    apt install apt-transport-https curl gnupg
-    curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-    echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    apt update
-    apt install brave-browser 
+# Brave
+apt install apt-transport-https curl gnupg -y
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+apt update
+apt install brave-browser -y
 
-    # FFmpeg
-    apt install ffmpeg 
+# FFmpeg
+apt install ffmpeg -y
 
-    # OBS
-    add-apt-repository ppa:obsproject/obs-studio 
-    apt install obs-studio 
-}
-
-yes | do_setup
+# OBS
+add-apt-repository ppa:obsproject/obs-studio 
+apt install obs-studio -y
